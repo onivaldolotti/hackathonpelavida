d2340de261b3f8797fa06ce35ce7428b004301a7
Onivaldo Alcides Lotti Junior <onivaldo_lotti@outloo.com>
2020-08-25 22:54:13 -0300

    Segundo dia

---
+++ front/app/Views/login/login.php
@@ -0,0 +1,16 @@
+<div id="container-login">
+    <div class="content" align="center">
+        <form method="POST" class="col-sm-4">
+            <div class="input-container">
+                <i class="fa fa-user icon"></i>
+                <input type="text" name="usuario" class="form-control" placeholder="Usuário"><br>
+            </div>
+            <div class="input-container">
+                <i class="fa fa-key icon"></i>
+                <input type="password" name="senha" class="form-control" placeholder="Senha"><br>
+            </div>
+            
+            <input type="submit" name="btn-Entrar" class="btn btn-outline-primary btn-lg btn-block">
+        </form>
+    </div>
+</div>
